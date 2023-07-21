5efdb4dd365b8b4eef2b1c81234d919181664b8d
Ethan <eethanccoon@gmail.com>
2023-07-20 14:23:51 -1000

    Added error message if bot is unable to query server

--- query.py
+++ query.py
@@ -2,10 +2,19 @@ from mctools import QUERYClient
 
 # The function to find basic stats for the server using the QUERY protocol
 def query_basic_stats(serverAddress, serverPort):
-    query = QUERYClient(serverAddress, serverPort)
-    return query.get_basic_stats()
+    try:
+        query = QUERYClient(serverAddress, serverPort, timeout = 10)
+        return query.get_basic_stats()
+    except Exception as e:
+        print(e)
+        return "The server is offline or I am searching the wrong address :("
+
 
 # The function to find more detailed stats for the server using the QUERY protocol
 def query_advanced_stats(serverAddress, serverPort):
-    query = QUERYClient(serverAddress, serverPort)
-    return query.get_full_stats()
+    try:
+        query = QUERYClient(serverAddress, serverPort, timeout = 10)
+        return query.get_full_stats()
+    except Exception as e:
+        print(e)
+        return "The server is offline or I am searching the wrong address :("
